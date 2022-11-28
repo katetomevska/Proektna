f8de64950c8ab11c2e22678f107235518c60e8fa
Katerina Tomevska <115409530+katetomevska@users.noreply.github.com>
2022-11-28 15:52:31 +0100

    Add files via upload

---
+++ js/index-K.js
@@ -0,0 +1,67 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
+
+navToggle.addEventListener("click", () => {
+        document.body.classList.toggle("nav-open");
+
+});
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        document.body.classList.remove("nav-open");
+
+    });
+
+});
+
+/* scroll up */
+function scrollUp(){
+    const scrollUp = document.getElementById("scroll-up");
+    if(this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove("show-scroll");
+}
+
+window.addEventListener("scroll", scrollUp);
+
+/* Dark ili Light tema */
+
+const themeButton = document.getElementById("social-list__item");
+const darkTheme = "dark-theme";
+const iconTheme = "fa-sun";
+
+const selectedTheme=localStorage.getItem("selected-theme");
+const selectedIcon=localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+ document.body.classList.contains(darkTheme) ? "dark" : "light";
+
+const getCurrentIcon = () =>
+ themeButton.classList.contains(iconTheme) ? "fa-moon" : "fa-sun";
+
+ if(selectedTheme){
+    document.body.classList[selectedTheme == "dark" ? "add" : "remove"](
+        darkTheme 
+    );
+    themeButton.classList[selectedIcon === "fa-moon" ? "add" : "remove"](
+        iconTheme
+    );
+ }
+
+ themeButton.addEventListener("click", () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    localStorage.setItem("selected-theme", getCurrentTheme ());
+    localStorage.setItem("selected-icon", getCurrentIcon());
+
+ });
+
+ const likeBtn=document.querySelectorAll(".like__btn");
+
+ likeBtn.forEach((btn) => {
+    btn.onclick = function () {
+        count = 0;
+        count += 1;
+        btn.innerHTML= '<i class="fas fa-thumbs-up"></i>' + " " + count + "Like";
+    };
+    
+ });
